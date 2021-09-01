21a2192b69c729be51b3957fa43231701d108cc5
Te1m0z <dmitriy.balamozhnov@gmail.com>
2021-09-01 10:00:45 +0400

    css mods

---
+++ src/Containers/App/App.jsx
@@ -0,0 +1,13 @@
+import s from './app.module.css';
+
+console.log(s);
+
+const App = () => {
+  return (
+    <div>
+      <h1>app</h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './Containers/App';
+
+import s from './styles/global.module.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);

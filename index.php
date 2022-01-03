c6b91abd92cbde218519a8c3b4eddb434844c5d0
te1m0z <dmitriy.balamozhnov@gmail.com>
2022-01-03 17:42:47 +0300

    просто вывод индекс файла

--- index.php
+++ index.php
@@ -12,10 +12,10 @@ header('Content-Type:text/html;charset=utf-8');
 # *действует пока не закроется браузер
 session_start();
 
-require_once 'config.php';
+// require_once 'config.php';
 
 # настройки для переноса сайта между хостингами
-require_once 'core/base/settings/internal_settings.php';
+// require_once 'core/base/settings/internal_settings.php';
 
 
-echo SITE_URL;
+echo 'da';

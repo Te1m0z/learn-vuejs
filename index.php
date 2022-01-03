644f22b3a62184cde5a3aa0963db54e6b75f916d
te1m0z <dmitriy.balamozhnov@gmail.com>
2022-01-03 21:03:09 +0300

    Подгрузка других классов spl

--- index.php
+++ index.php
@@ -14,11 +14,17 @@ header('Content-Type:text/html;charset=utf-8');
 # *действует пока не закроется браузер
 session_start();
 
-// require_once 'config.php';
+require_once 'config.php';
 
 # настройки для переноса сайта между хостингами
-// require_once 'core/base/settings/internal_settings.php';
+require_once 'core/base/settings/internal_settings.php';
 
-// var_dump(new n1\A());
+function loadAPI($name)
+{
+    $name = str_replace('\\', '/', $name);
+    include $name.'.php';
+}
 
-echo 'Hellow orld 2';
+spl_autoload_register('loadAPI');
+
+// var_dump(new \API\Start());
